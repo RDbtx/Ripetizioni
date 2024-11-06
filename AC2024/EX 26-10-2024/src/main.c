6e010509e16eff0ea71b5821d87a2608f572dd25
Riccardo Deidda <127761846+RDbtx@users.noreply.github.com>
2024-11-06 21:25:37 +0100

    Update main.c

--- AC2024/EX 26-10-2024/src/main.c
+++ AC2024/EX 26-10-2024/src/main.c
@@ -3,13 +3,8 @@
 #include "TUTORATO1.h"
 #include "TUTORATO2.h"
 #include "TUTORATO3.h"
+#include "TUTORATO4.h"
 #include "FOR_STRINGS.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
 
 int main(void) {
     return 0;
