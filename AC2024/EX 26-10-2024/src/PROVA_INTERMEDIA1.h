1fede57c13d328bed2112158d66e874cbb865281
Riccardo Deidda <127761846+RDbtx@users.noreply.github.com>
2024-11-08 15:10:26 +0100

    Add files via upload

---
+++ AC2024/EX 26-10-2024/src/PROVA_INTERMEDIA1.h
@@ -0,0 +1,18 @@
+//
+// Created by ricca on 08/11/2024.
+//
+
+#ifndef PROVA_INTERMEDIA1_H
+#define PROVA_INTERMEDIA1_H
+
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+
+void lvl0_ex3();
+void lvl1_ex2();
+char lvl2_ex();
