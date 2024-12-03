67b097de273eeab0fcb53677e3a1019519c57694
Riccardo Deidda <127761846+RDbtx@users.noreply.github.com>
2024-12-03 08:44:47 +0100

    Create FUNZIONI.h

---
+++ AC2024/EX 26-10-2024/src/FUNZIONI.h
@@ -0,0 +1,39 @@
+#ifndef FUNZIONI_H
+#define FUNZIONI_H
+
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+
+//EX1:
+int sum(const int a, const int b);
+
+//EX2:
+int quadrato(const int n);
+
+//EX3:
+int isPari(const int n);
+
+//EX4:
+int isDivisible(const int a, const int b);
+
+//EX5:
+int minimo(const int a, const int b);
+
+//EX6:
+int fattoriale(int n);
+
+//EX7:
+void invertiStringa(char *str, const int len);
+
+//EX8:
+int massimo(const int *arr, const int size);
+
+//EX9:
+void FizzBuzz(const int val, const int div1, const int div2);
+
+bool FBisDivisible(const int val, const int div);
